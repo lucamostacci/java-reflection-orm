8ed265b4372f76c48780e216c05dfbced158e422
lucamostacci <luca.mostacci@gmail.com>
2026-04-15 11:40:31 +0200

    testati tutti i metodi CRUD nel Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,7 @@ public class Main {
         Class.forName("org.sqlite.JDBC");
         Connection connection = DriverManager.getConnection("jdbc:sqlite:database.db");
         EntityManager entityManager = new EntityManager(connection);
-        Utente utente = new Utente("Luca","luca.mostacci@gmail.com",30);
+        Utente utente = new Utente("Mario","mario.rossi@gmail.com",28);
 
         connection.createStatement().execute("CREATE TABLE IF NOT EXISTS utenti (id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, email TEXT, eta INTEGER)");
 
@@ -17,6 +17,19 @@ public class Main {
 
         System.out.println("Utente salvato!");
         Utente trovato = entityManager.findById(Utente.class, 1L);
-        System.out.println("Trovato: " + trovato.getNome() + " - " + trovato.getEmail());
+        trovato.setEmail("nuovaMail@gmail.com");
+        entityManager.update(trovato);
+        System.out.println("Email aggiornata in " + trovato.getEmail());
+
+        Utente aggiornato = entityManager.findById(Utente.class, 1L);
+        System.out.println("Verifica: " + aggiornato.getEmail());   
+
+        entityManager.delete(Utente.class, 1L);
+        Utente cancellato = entityManager.findById(Utente.class, 1L);
+        if (cancellato == null) {
+            System.out.println("Utente cancellato correttamente!");
+        } else {
+            System.out.println("Qualcosa è andato storto!");
+        }
     }
 }
